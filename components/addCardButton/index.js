ed496fe42c68ba345ad7658e5abb2dafb9510738
Emad <ejyption@gmail.com>
2019-08-12 21:07:46 +1200

    Changed button icon display function

--- components/addCardButton/index.js
+++ components/addCardButton/index.js
@@ -2,36 +2,35 @@ import React from 'react';
 import { TouchableHighlight, Animated, Dimensions } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
-let {width:winWidth, height:winHeight} = Dimensions.get('window');
+let { width: winWidth, height: winHeight } = Dimensions.get('window');
 
 console.log(winWidth);
 let buttonAnim = new Animated.Value(0);
-let buttonAbsolutePosition = buttonAnim.interpolate({ inputRange:[0,1], outputRange:[50, 0] });
-let buttonHeight = buttonAnim.interpolate({ inputRange:[0,1], outputRange:[50, winHeight] });
-let buttonWidth = buttonAnim.interpolate({ inputRange:[0,1], outputRange:[50, winWidth] });
-let buttonBR = buttonAnim.interpolate({ inputRange:[0,1], outputRange:[25, 0] });
-let buttonBC = buttonAnim.interpolate({ inputRange:[0,1], outputRange:["#8711EF","#FFFF00"] });
+let buttonAbsolutePosition = buttonAnim.interpolate({ inputRange: [0, 1], outputRange: [50, 0] });
+let buttonHeight = buttonAnim.interpolate({ inputRange: [0, 1], outputRange: [50, winHeight] });
+let buttonWidth = buttonAnim.interpolate({ inputRange: [0, 1], outputRange: [50, winWidth] });
+let buttonBR = buttonAnim.interpolate({ inputRange: [0, 1], outputRange: [25, 0] });
+let buttonBC = buttonAnim.interpolate({ inputRange: [0, 1], outputRange: ["#8711EF", "#FFFF00"] });
 
 let AnimatedTH = new Animated.createAnimatedComponent(TouchableHighlight);
 
 export default class AddCardButton extends React.Component {
 	constructor() {
 		super();
-		this.state = { display: "display:block" }
+		this.state = { display: "block" }
 	}
 	changeDisplay = () => {
-		this.setState({display:"display:none"});
-		console.log(this.state);
+		this.setState({ display: "none" });
 	}
 	addButtonPressed = () => {
-		Animated.timing( buttonAnim, { toValue:1, duration:500 } ).start();
+		Animated.timing(buttonAnim, { toValue: 1, duration: 500 }).start();
 	}
 	render() {
 		return (
 			<AnimatedTH underlayColor="#FFFF00" style={addButtonStyle} onPress={this.addButtonPressed}>
-                <Feather color="#FFF" size={28} name="plus" style={this.state.display}/>
-            </AnimatedTH>
-    	);
+				<Feather color="#FFF" size={28} name="plus" style={{ display: this.state.display }} />
+			</AnimatedTH>
+		);
 	}
 
 }
@@ -42,8 +41,8 @@ const addButtonStyle = {
 	right: buttonAbsolutePosition,
 	backgroundColor: buttonBC,
 	borderRadius: buttonBR,
-	width:buttonWidth,
-	height:buttonHeight,
+	width: buttonWidth,
+	height: buttonHeight,
 	alignItems: 'center',
 	justifyContent: 'center',
 	shadowColor: '#000',
