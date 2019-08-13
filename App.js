9a20c0382dac08481a865b653653f8b3fd103239
keithpaul <keith_paul@outlook.com>
2019-08-13 15:45:40 +1200

    Plus Button

--- App.js
+++ App.js
@@ -1,32 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, } from 'react-native';
+import { StyleSheet, Text, View, } from 'react-native';
 import AddCardButton from './components/addCardButton';
 
 export default class App extends React.Component {
 	constructor() {
 		super();
-		this.state = { title: "Keith is the man",
-						time: "NOW", 
-						inputs: {
-							question: "",
-							answer: "",
-						} 
-					};
+		this.state = { title: "Keith is the man", time: "NOW" }
 	}
 
 	render() {
 		return (
 			<View style={styles.container}>
-				<View style={{width: 300, height: 100, backgroundColor: '#FFFFFF', borderRadius: 4, padding: 12}}>
-            		<Text style={{fontSize: 14}}>Question:</Text>
-            		<TextInput style={{fontSize: 16}} onChangeText={(question) => this.setState({question})} />
-					<Text>{this.state.question}</Text>					
-          		</View>
-				<View style={{width: 300, height: 300, backgroundColor: '#FFFFFF', borderRadius: 4, marginTop: 25, padding: 12}}>
-					<Text style={{fontSize: 14}}>Answer:</Text>
-            		<TextInput style={{fontSize: 16}} onChangeText={(answer) => this.setState({answer})} />
-					<Text>{this.state.answer}</Text>
-				</View>
+				<Text>{this.state.title}</Text>
+				<Text>{this.state.time}</Text>
 				<AddCardButton />
 			</View>
 		);
@@ -37,9 +23,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#231F40',
+		backgroundColor: '#fff',
 		alignItems: 'center',
 		justifyContent: 'center',
 	},
 });
-
